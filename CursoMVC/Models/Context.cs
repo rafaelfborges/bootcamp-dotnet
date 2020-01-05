9e1ff35b20fe15245335d1355397fd7dfe86cd42
Rafael Borges <rafaelfborges@gmail.com>
2020-01-05 13:59:45 -0300

    Add project files.

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CursoMVC.Models;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+        }
+
+        public DbSet<CursoMVC.Models.Produto> Produto { get; set; }
+    }
+}
