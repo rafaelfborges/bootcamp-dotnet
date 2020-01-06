695a08b691d029eb3ecfe7746644706f44e5316e
Rafael Borges <rafaelfborges@gmail.com>
2020-01-05 23:40:24 -0300

    Adicionado testes para os métodos Put, Post e Delete da API

--- CursoTest/CategoriasControllerTest.cs
+++ CursoTest/CategoriasControllerTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using CursoAPI.Controllers;
 using CursoMVC.Models;
@@ -22,6 +23,11 @@ namespace CursoTest
             _mockContext.Setup(m => m.Categorias).Returns(_mockSet.Object);
             _mockContext.Setup(m => m.Categorias.FindAsync(1))
                 .ReturnsAsync(_categoria);
+
+            _mockContext.Setup(m => m.SetModified(_categoria));
+
+            _mockContext.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(1);
         }
         
         [Fact]
@@ -31,6 +37,40 @@ namespace CursoTest
             await service.GetCategoria(1);
 
             _mockSet.Verify(m => m.FindAsync(1), 
+                Times.Once());
+        }
+
+        [Fact]
+        public async Task Put_Categoria()
+        {
+            var service = new CategoriasController(_mockContext.Object);
+            await service.PutCategoria(1, _categoria);
+
+            _mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), 
+                Times.Once());
+        }
+
+        [Fact]
+        public async Task Post_Categoria()
+        {
+            var service = new CategoriasController(_mockContext.Object);
+            await service.PostCategoria(_categoria);
+
+            _mockSet.Verify(x => x.Add(_categoria), Times.Once());
+            _mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), 
+                Times.Once());
+        }
+
+        [Fact]
+        public async Task Delete_Categoria()
+        {
+            var service = new CategoriasController(_mockContext.Object);
+            await service.DeleteCategoria(1);
+
+            _mockSet.Verify(m => m.FindAsync(1), 
+                Times.Once());
+            _mockSet.Verify(x => x.Remove(_categoria), Times.Once);
+            _mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), 
                 Times.Once());
         }
     }
